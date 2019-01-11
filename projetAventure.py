2027bc67027382681d01f98d04ad630f77a2789a
diggersheep <alexandre.combeau@outlook.fr>
2019-01-11 09:09:06 +0100

    Added class

--- projetAventure.py
+++ projetAventure.py
@@ -6,13 +6,36 @@ from typing import Union
 from typing import Callable
 from typing import Any
 
-def load_properties(filepath : str) -> Any:
-    try:
-        with open(filepath, 'r') as f:
-            ppt = json.load(f)
-        return ppt
-    except IOError as e:
-        print('IOERROR - {}'.format(str(e)))
+
+class TextAdventure:
+    def __init__(self, filename: str, start: str):
+        self.filename = filename
+        self.ppt = self.load_properties()
+
+    def load_properties(self) -> Any:
+        try:
+            with open(self.filename, 'r') as f:
+                ppt = json.load(f)
+            return ppt
+        except IOError as e:
+            print('IOERROR - {}'.format(str(e)))
+            exit(1)
+
+    
+
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("No text file for text adventure :'(")
+        print("Please enter a file.")
         exit(1)
 
+    filename = sys.argv[1]
+    #start = 'Intro'
+    start = "foo"
 
+    t = TextAdventure(filename, start)
+    t.load_properties(  
+    print(json.dumps(t.ppt, indent=2))
